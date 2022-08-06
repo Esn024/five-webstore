feb61fbef96e0d18916ef640138469e9f211e8a9
Eugene Belianski <i_esn_i@yahoo.ca>
2022-08-06 16:52:40 -0400

    remove strict mode, to prevent double-actions

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,8 @@ import App from "./components/App";
 import { ShopDataProvider } from "../src/components/ShopDataContext"; //Add the Context Provider to the entire application
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ShopDataProvider>
-      <App />
-    </ShopDataProvider>
-  </React.StrictMode>,
+  <ShopDataProvider>
+    <App />
+  </ShopDataProvider>,
   document.getElementById("root")
 );
